34fc5afd0da1ddc5ac0219671c52b8b0eaed7b87
Roberto Artigues Escobar <rartigues2019@inf.udec.cl>
2023-10-02 23:53:10 -0300

    ✨ Diablillito de procesos listo

--- main.cpp
+++ main.cpp
@@ -3,6 +3,64 @@
 #include <vector>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <syslog.h>
+#include <fstream>
+#include <string>
+
+// Implementar el demonio
+void start_daemon(int t, int p) {
+	pid_t pid = fork(); // Se crea un proceso hijo
+	if (pid == 0) {
+		umask(0); // Cambiar la máscara de modo de archivo, para que los archivos creados por el demonio tengan todos los permisos
+		setsid(); // Nueva sesión
+		close(STDIN_FILENO);
+		close(STDOUT_FILENO);
+		close(STDERR_FILENO);
+
+		openlog("SusDaemon", LOG_PID, LOG_DAEMON); // Iniciar el log
+		syslog(LOG_NOTICE, "El diablillo se ha creado"); // Mensaje de inicio
+
+		for (int i = 0; i < p / t; ++i) {
+            syslog(LOG_NOTICE, "Diablillo corriendo por la %d vez", i + 1);
+			std::ifstream file("/proc/stat"); // Sacando la info desde /proc/stat porque /proc/cpuinfo no tiene la info de procesos (Al menos en WSL Ubuntu)
+            // std::ifstream file("/proc/cpuinfo"); //! Descomentar esta línea para probar con /proc/cpuinfo
+			std::string line;
+			int processes = 0, procs_running = 0, procs_blocked = 0;
+
+			if (file.is_open()) {
+				while (std::getline(file, line)) {
+					if (line.find("processes") != std::string::npos) {
+						std::istringstream iss(line);
+						std::string key;
+						iss >> key >> processes;
+					}
+					if (line.find("procs_running") != std::string::npos) {
+						std::istringstream iss(line);
+						std::string key;
+						iss >> key >> procs_running;
+					}
+					if (line.find("procs_blocked") != std::string::npos) {
+						std::istringstream iss(line);
+						std::string key;
+						iss >> key >> procs_blocked;
+					}
+				}
+				file.close();
+			}
+
+			syslog(LOG_NOTICE, "Procesos: %d, Corriendo: %d, Bloqueados: %d", processes, procs_running, procs_blocked);
+			sleep(t);
+		}
+        syslog(LOG_NOTICE, "Diablillito ha terminado");
+		closelog(); // Cerrar el log
+        exit(0);
+	}
+}
+
+
 
 int main() {
     while (true) {
@@ -41,6 +99,33 @@ int main() {
         }
         // Agregar el último comando al vector de comandos
         commands.push_back(args);
+
+        if (commands[0][0] == "start_daemon") {
+            if (commands[0].size() != 3) {
+                std::cerr << "Error: start_daemon requiere exactamente 2 argumentos (t y p).\n";
+            } else {
+                try { // Convertir los argumentos a int
+                    int t = std::stoi(commands[0][1]); 
+                    int p = std::stoi(commands[0][2]); 
+
+                    if (t <= 0 || p <= 0) {
+                        std::cerr << "Error: t y p deben ser números enteros positivos.\n";
+                    }  else {
+                        if (t > p) {
+                            std::cerr << "Error: t debe ser menor o igual a p.\n";
+                            continue;
+                        }
+                        start_daemon(t, p); // Llamar a la función para iniciar el demonio
+                        continue;
+                    }
+                } catch (const std::invalid_argument& e) {
+                    std::cerr << "Error: Los argumentos t y p deben ser números enteros.\n";
+                } catch (const std::exception& e) {
+                    std::cerr << "Error desconocido: " << e.what() << '\n';
+                }
+            }
+            continue;
+        }
 
         // Crear un vector de pipes
         std::vector<int[2]> pipes(commands.size() - 1);
